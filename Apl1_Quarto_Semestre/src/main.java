cbf0dc9ddc7514e8b6d60985cb09067f1a388a05
Enzo-B-Proenca <silver200347@gmail.com>
2024-09-09 18:22:33 -0300

    Adição do Menu e validador de entrada

--- Apl1_Quarto_Semestre/src/main.java
+++ Apl1_Quarto_Semestre/src/main.java
@@ -1,8 +1,46 @@
+import java.util.Scanner;
 
 public class main {
+  public static validateInput(String input){
+    return input.matches("[0-9\\+-*/().\\]+");
+  }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    Scanner scanner = new Scanner(System.in);
+    
+    // Decidir se o menur deve aparecer várias vezes.
+    System.out.println("1.\n");
+    System.out.println("2.\n");
+    System.out.println("3.\n");
+    System.out.println("4.\n");
+    System.out.println("5.\n");
+    System.out.println("Informe o número de sua escolha: ");
+    scanner.nextLine();
+
+    while (true) {
+      switch (scanner) {
+        case "1":
+          try {
+
+            System.out.println("Informe a expressão infixa: \n");
+            scanner.nextLine();
+
+            if (validateInput(scanner)){
+              System.out.println("Entrada válida.\n");
+            } else {
+                System.out.println("Entrada inválida. Use somente número (inteiros ou decimais), operadores matemáticos (+, *, -, /) e parênteses.");
+                throw new InputMismatchException("Entrada inválida.") 
+              }
+
+          } catch (Exception e) {
+            System.out.println("Deu erro patrão.");
+          }
+          break;
+        default:
+          System.out.println("Escolha inválida.");
+          break;
+      }
+    }
 
 	}
 
